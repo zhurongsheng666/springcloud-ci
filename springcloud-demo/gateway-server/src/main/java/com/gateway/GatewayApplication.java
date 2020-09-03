ea81e8cb972162c1a2e40f721d3763c4406446e7
zhurongsheng <zhurongsheng666@163.com>
2020-09-03 23:53:48 +0800

    持续集成

---
+++ springcloud-demo/gateway-server/src/main/java/com/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description:
+ * @Author: Rosh
+ * @Date: 2020/8/15 17:52
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
---
+++ springcloud-demo/student-server/src/main/java/com/student/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.student.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: Rosh
+ * @Date: 2020/8/15 20:55
+ */
+@RestController
+public class StudentController {
+
+
+    @GetMapping("/index")
+    public String index() {
+
+        return "this is student server 1 2 3 4 5";
+    }
+
+
+}
